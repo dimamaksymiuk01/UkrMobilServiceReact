bbfc12edceb98fc5e20926fd4ef4ddf8162adb50
Anton Halaburda <a.halaburda@brainence.com>
2023-05-15 02:05:57 +0300

    works?

---
+++ UkrMobilServiceReact/Program.cs
@@ -0,0 +1,45 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using UkrMobilServiceNotes2.Data;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+
+var builder = WebApplication.CreateBuilder();
+
+
+builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddFluentValidationAutoValidation();
+// builder.Services.Configure<ApiBehaviorOptions>(options =>
+//             {
+//                 options.SuppressModelStateInvalidFilter = true;
+//             });
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=UkrMobilNotes.db"));
+builder.Services.AddTransient<INotesRepository, NotesRepository>();
+builder.Services.AddControllers();
+builder.Services.AddSpaStaticFiles(configuration => configuration.RootPath = "frontend/build");
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+app.UseSpaStaticFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors(builder =>
+    builder
+     .AllowAnyOrigin()
+     .AllowAnyHeader()
+     .AllowAnyMethod()
+);
+
+app.UseStaticFiles();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+app.UseSpa(spa => spa.Options.SourcePath = "frontend");
+app.Run();
